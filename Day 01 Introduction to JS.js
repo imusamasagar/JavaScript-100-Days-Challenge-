990ab44f74be52704cb121307fd6a434610135e0
imusamasagar <usamaist2747@gmail.com>
2023-10-24 21:07:41 +0500

    Day 1

---
+++ Day 01 Introduction to JS.js
@@ -0,0 +1,77 @@
+// JavaScript, often abbreviated as JS, is a programming language that is one of the core technologies of the World Wide Web, alongside HTML and CSS. As of 2023, 98.7% of websites use JavaScript on the client side for webpage behavior, often incorporating third-party libraries.
+
+// Content
+// Variables
+// const / let /var
+// primitives and objects
+// Operators
+// expressions
+// consitional statements
+// for loop
+// while loop
+// do while loop
+// functions
+// string and methods
+// array with methods
+// array with loops
+// map filter and reduce
+// js in browser
+// script tag
+// console object
+// alert prompt confirm
+// bom dom window object
+// Walking the DOM
+// assesing chilrdren
+// parents and siblings elements
+// navigation
+// seraching the DOM
+// matches closest contains
+// innerhtml
+// outerhtml
+// insertion methods
+// insertadjacent html
+// insertadjacentelements
+// change html classes
+// setinterval
+// settimeout
+// browser events
+// handling browser events
+// Asynchronous JS
+// Callbacks
+// Callback HEll
+// Promises
+// then and catch
+// handlers attach
+// promise API
+// async await
+// error handling 
+// try catch
+// 	error object 
+// custom objects
+// finaly clause
+// fetch API
+// Sending POST
+// cookies
+// local storage
+// sessionj storage
+// methods
+// OOP
+// prototype and proto
+// classes and objects
+// constructor
+// inheritance
+// overriding
+// static method
+
+
+
+// Print some thing on the console
+console.log("Usama Sagar write a code to add numbers")
+// function to add numbers
+function sum(a,b,c){
+  return a + b +c;
+}
+// call function
+let c = sum(25,21,48)
+// print answer on console
+console.log(c)
