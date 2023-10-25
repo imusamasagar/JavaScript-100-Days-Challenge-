f98dc3f4242631df3c490d3ff5eaa00383bfa906
imusamasagar <usamaist2747@gmail.com>
2023-10-25 12:59:48 +0500

    Day 2

---
+++ Day 02 Variables.js
@@ -0,0 +1,17 @@
+console.log("Lecture 2 Variables")
+// var used in past to create varibale
+// let is used now to create varibale 
+let a=96;
+console.log(a)
+// you can change the data type in current time 
+// eralier i declare a as a number
+// let take it as a string now
+ a = "harry"
+console.log(a)
+// practice 
+function evenno(a)
+  {
+    return a/2;
+  }
+a = evenno(10)
+console.log(a)
