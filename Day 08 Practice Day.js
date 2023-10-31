b1486110eec345fa2d2762d36e8093d0dd35d8ee
imusamasagar <usamaist2747@gmail.com>
2023-10-31 17:17:46 +0500

    Day 8

--- Day 08 Practice Day.js
+++ Day 08 Practice Day.js
@@ -10,3 +10,23 @@ if (age1 > 10 && age1 < 20) {
 else {
   console.log("Your age doesnt lies between 10 and 20")
 }
+
+// Problem No 2
+// switch cases ???
+let age2 = prompt("What is your age?")
+switch (age2) {
+  case '12':
+    console.log("Your age is 12")
+    break
+  case '13':
+    console.log("Your age is 13")
+    break
+  case '14':
+    console.log("Your age is 14")
+    break
+  case '15':
+    console.log("Your age is 15")
+    break
+  default:
+    console.log("Your age is not special")
+}
