f16cfb7304a44caf283db69df0296cded7aa91b1
imusamasagar <usamaist2747@gmail.com>
2023-10-31 17:16:42 +0500

    Day 8

---
+++ Day 08 Practice Day.js
@@ -0,0 +1,12 @@
+// Chapter 2
+
+// Problem No 1
+// age lies between 10 and 20 ???
+let age1 = prompt("What is your age?")
+age1 = Number.parseInt(age1)
+if (age1 > 10 && age1 < 20) {
+  console.log("Your age lies between 10 and 20")
+}
+else {
+  console.log("Your age doesnt lies between 10 and 20")
+}
