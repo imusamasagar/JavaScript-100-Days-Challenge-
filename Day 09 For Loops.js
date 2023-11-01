1ad731d5282421f8977daca313a16ff20374dece
imusamasagar <usamaist2747@gmail.com>
2023-11-01 10:04:51 +0500

    Day 9

---
+++ Day 09 For Loops.js
@@ -0,0 +1,17 @@
+// Lecture 09 for loops
+// for loop consist of three parts initialize condition increment 
+// for loop execute till the condition is true 
+
+for(let a =0; a<=10; a++)
+  {
+    console.log(a)
+  }
+console.log("Table of 2")
+let b;
+for(b=1; b<=20; b++)
+  // * this is multiply sign
+  // + operator is used to indicate the space and also seperate the terms 
+  {
+    console.log("2 x "+b+" = "+ 2*b)
+  }
+
