c191cdcb1e5f612fbc48c8fca83ebbbe5e416121
imusamasagar <usamaist2747@gmail.com>
2023-11-02 23:03:13 +0500

    Day 10

--- Day 10 While Loop.js
+++ Day 10 While Loop.js
@@ -10,3 +10,13 @@ while(i<n){
   console.log(i)
   i++;
 }
+// do while loop
+// this loop body should execute one tome minimum either the condition is true or not bcz in do while loop the condition is placed after the body statements
+
+let m = prompt("Enter the value of m")
+m = Number.parseInt(m) 
+let j = 10;
+do{
+  console.log(j)
+  j++;
+}while (j < m) 
