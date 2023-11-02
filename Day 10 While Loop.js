308499bfa1b4554404210d7af4ef33a98a8cc2a2
imusamasagar <usamaist2747@gmail.com>
2023-11-02 23:02:46 +0500

    Day 10

--- Day 10 While Loop.js
+++ Day 10 While Loop.js
@@ -3,3 +3,10 @@
 // while loop
 let n = prompt("Enter the value of n")
 n = Number.parseInt(n)
+// syntax is as initialize outside the bracket then while with a condition ahead then after bracket increment the while loop can run untill the condition is false do if you give that condition that dont false your code will crash
+
+let i = 0;
+while(i<n){
+  console.log(i)
+  i++;
+}
