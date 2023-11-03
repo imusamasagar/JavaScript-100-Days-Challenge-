fd5cafe355965b72f8cb95ab64f46f1188bf89b3
imusamasagar <usamaist2747@gmail.com>
2023-11-03 23:06:12 +0500

    Day 11

--- Day 11 Functions.js
+++ Day 11 Functions.js
@@ -1,3 +1,14 @@
-// What is Function?
+// Lecture 11 functions
 
+// What is Function?
 /*A function in JavaScript is similar to a procedure—a set of statements that performs a task or calculates a value, but for a procedure to qualify as a function, it should take some input and return an output where there is some obvious relationship between the input and the output.*/
+
+// used when multiple lines of code used repeatedly in the code
+// avoid repentance of code
+// should call or invoke the function otherwise they will not execute
+// function with parameters 
+// function without parameters / argument 
+// functio return jo karta he call karne par wo cheez print or execute ho jati he
+/*
+Math.round ye use karte he value ko round of karne k liye java script me
+*/
