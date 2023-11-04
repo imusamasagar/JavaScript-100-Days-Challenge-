faac09e6eabf63000ec19450563db416b05f6cba
imusamasagar <usamaist2747@gmail.com>
2023-11-04 22:42:18 +0500

    Day 12

---
+++ Day 12 Practice Day.js
@@ -0,0 +1,11 @@
+// Chapter 3 Practise set
+
+// Question no 1
+// Create object and print marks of student with the use of simple for loop
+// here i make a obj marks 
+let marks = {
+    usama: 40,
+    Ali: 50,
+    Ahmad: 60,
+  }
+  
