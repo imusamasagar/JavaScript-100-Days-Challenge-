b77f994450048adffd3643400242daee9e942bb0
imusamasagar <usamaist2747@gmail.com>
2023-11-05 23:43:44 +0500

    Day 13

--- Day 13 String.js
+++ Day 13 String.js
@@ -10,3 +10,12 @@ console.log(name1)
 // you can aslo write your string in double quotes 
 let name2 = "ali"
 console.log(name2)
+
+// imp you cant write sigle at one end and double at other end use same for both start and end 
+
+// template literal
+// important used in modren java script
+// ye allow karte he k iss k andar beshak single ya double quote wali string daal sakte he 
+// backtick use hota he syntax me iss k 
+let name3 = `hello "ahmad" 'here'`
+console.log(name3)
