4f77037a2a0efe577260e6b1b4a6790dab165d32
imusamasagar <usamaist2747@gmail.com>
2023-11-05 23:43:08 +0500

    Day 13

--- Day 13 String.js
+++ Day 13 String.js
@@ -5,3 +5,8 @@
 // you can write the string in single quotes 
 let name1 = 'Usama'
 console.log(name1)
+
+// double quote 
+// you can aslo write your string in double quotes 
+let name2 = "ali"
+console.log(name2)
