d4bcc6a290ef8c196cf565f8916571c7f4cde1b9
imusamasagar <usamaist2747@gmail.com>
2023-11-06 21:44:56 +0500

    Day 14

--- Day 14 String Methods.js
+++ Day 14 String Methods.js
@@ -14,3 +14,9 @@ console.log(name.toUpperCase())
 
 // it will convert all letters to lower case
 console.log(name.toLowerCase())
+
+// it will print the leeter from index 2 to 3 4 is not included
+console.log(name.slice(2, 4))  
+
+// it will print the leeter from index 2 to end of string
+console.log(name.slice(2))
