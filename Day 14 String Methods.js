8792847ab25c4bef514e4c3d639b3fcba17f1d62
imusamasagar <usamaist2747@gmail.com>
2023-11-06 21:44:32 +0500

    Day 14

--- Day 14 String Methods.js
+++ Day 14 String Methods.js
@@ -8,3 +8,9 @@ console.log(name.length)
 // \" it will consider as a single character 
 let name2 = "Usama \" Sagar"
 console.log(name2.length)
+
+// it will convert all letters to upper case
+console.log(name.toUpperCase())
+
+// it will convert all letters to lower case
+console.log(name.toLowerCase())
