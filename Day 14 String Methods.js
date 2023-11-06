eede9bb2d138474355ea5a13613360ceab323fbf
imusamasagar <usamaist2747@gmail.com>
2023-11-06 21:43:53 +0500

    Day 14

---
+++ Day 14 String Methods.js
@@ -0,0 +1,10 @@
+// Lecture 14 methods of strings
+
+let name = "Usama Sagar"
+
+// to find the length of the string
+console.log(name.length)
+
+// \" it will consider as a single character 
+let name2 = "Usama \" Sagar"
+console.log(name2.length)
