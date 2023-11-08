855007ad24df1f863ca6bcb905a9d5303a48a2d1
imusamasagar <usamaist2747@gmail.com>
2023-11-08 21:14:43 +0500

    Day 16

--- Day 16 Arrays.js
+++ Day 16 Arrays.js
@@ -5,3 +5,13 @@
 // Can be changed
 // Diffrent type of data can store
 // index start with 0
+
+let marks = [91, 82, 63, 84, false, "Not Present"]
+// Access the alues from the index
+console.log(marks[0])
+console.log(marks[1])
+console.log(marks[2])
+console.log(marks[3])
+console.log(marks[4])
+console.log(marks[5])
+console.log(marks[6]) // Will be undefined because index 6 does not exist
