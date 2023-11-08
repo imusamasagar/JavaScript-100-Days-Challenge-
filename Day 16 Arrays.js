e9e97e576bb32f4f5ab0024efc7755f1ba6e319f
imusamasagar <usamaist2747@gmail.com>
2023-11-08 21:14:14 +0500

    Day 16

---
+++ Day 16 Arrays.js
@@ -0,0 +1,7 @@
+// Day 16 Arrays
+// Arrays is a variable consist of many items
+// collection of items
+// Can be editable
+// Can be changed
+// Diffrent type of data can store
+// index start with 0
