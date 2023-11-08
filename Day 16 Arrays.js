36c9522d1025b508884a78693cd6d62e1db69466
imusamasagar <usamaist2747@gmail.com>
2023-11-08 21:18:38 +0500

    Day 16

--- Day 16 Arrays.js
+++ Day 16 Arrays.js
@@ -36,3 +36,5 @@ for(let a =0; a<array.length; a++)
   {
     console.log(array[a])
   }
+// Practice Q
+// Write a JavaScript program that accepts a number as input and inserts dashes (-) between each even number. For example if you accept 025468 the output should be 0-254-6-8.
