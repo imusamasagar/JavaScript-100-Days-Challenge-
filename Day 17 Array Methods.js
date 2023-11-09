839f81daa7649ab319e2c18699d227efb565ea1e
imusamasagar <usamaist2747@gmail.com>
2023-11-09 11:22:07 +0500

    Day 17

--- Day 17 Array Methods.js
+++ Day 17 Array Methods.js
@@ -1,5 +1,14 @@
 // Lecture 17 Array Methods
 let num = [1, 2, 3, 34, 4]
 console.log(num)
+
 // covert array to a string of comma separated 
 let b = num.toString() // b is now a string 
+
+// b to print the string and typeof to check if it will covert correctly
+console.log(b, typeof b)
+
+// it may format your array and one imp thing the join ethod also covert the array to string automatically 
+// here " and " this will be present between all the values
+let c = num.join(" and ")
+console.log(c, typeof c)
