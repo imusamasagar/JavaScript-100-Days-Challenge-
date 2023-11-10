9ec92921f0ffce5acdab6d2e3e1305e35c1d2969
imusamasagar <usamaist2747@gmail.com>
2023-11-10 12:06:30 +0500

    Day 18

--- Day 18 Array Methods 2.0.js
+++ Day 18 Array Methods 2.0.js
@@ -3,3 +3,8 @@
 let num1 = [1, 2, 3, 4, 5, 6, 7, 8, 9]
 let num2= [11, 12, 13, 14, 15, 16, 17, 18, 19]
 let num3 = [211, 212, 213, 214, 415, 416, 417, 418, 419]
+// print the length of array
+console.log(num1.length)
+console.log(num2.length)
+console.log(num3.length)
+
