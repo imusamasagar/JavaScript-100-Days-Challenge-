ea2f4f0b601bd2e230188f51c0e46b9df5c21f38
Usama Sagar <134643424+imusamasagar@users.noreply.github.com>
2023-11-11 14:54:31 +0500

    Day 19

--- Day 19 Looping Array.js
+++ Day 19 Looping Array.js
@@ -5,3 +5,11 @@ let num = [3, 5, 1, 2, 4]
 for(let i=0; i<num.length;i++){
   console.log(num[i])
 }
+
+// ForEach Loop
+// here we make a arrow function element which can ultiply the item with its self and print it  
+// for each means run for each elemnt of the array 
+// help in react based projects
+num.forEach((element) => {
+  console.log(element * element)
+})
