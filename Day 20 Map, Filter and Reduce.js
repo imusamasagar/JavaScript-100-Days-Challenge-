0539ab8da553c9903fa928ed8ad8fb1206343e22
imusamasagar <usamaist2747@gmail.com>
2023-11-12 12:57:38 +0500

    Day 20

---
+++ Day 20 Map, Filter and Reduce.js
@@ -0,0 +1,7 @@
+// Lecture 20 map , filter , reduce
+// higher order array methods
+// less time consuming 
+// useful modern JavaScript 
+// orignal array will not modified always important 
+// ye sare aik new array formed karte he
+
