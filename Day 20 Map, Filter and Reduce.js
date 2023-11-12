ac730de1507abba8d52d76f91404420a0bfde87c
imusamasagar <usamaist2747@gmail.com>
2023-11-12 12:58:20 +0500

    Day 20

--- Day 20 Map, Filter and Reduce.js
+++ Day 20 Map, Filter and Reduce.js
@@ -8,3 +8,9 @@
 // map
 // diffrence between for each and map is that mep will return a new array by perfprm some operation on existing array -- for each is just doing some function or operation on each element of the array 
 
+let l = [21,54,87]
+l.map((value)=>{
+  // it will just print the value
+  console.log(value)
+})
+
