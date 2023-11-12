c1b37e2c52da87b5afd314ff6cba2b93cb29ec21
imusamasagar <usamaist2747@gmail.com>
2023-11-12 12:58:00 +0500

    Day 20

--- Day 20 Map, Filter and Reduce.js
+++ Day 20 Map, Filter and Reduce.js
@@ -5,3 +5,6 @@
 // orignal array will not modified always important 
 // ye sare aik new array formed karte he
 
+// map
+// diffrence between for each and map is that mep will return a new array by perfprm some operation on existing array -- for each is just doing some function or operation on each element of the array 
+
