b49fbf9571fe1be5543e39ef312091ca4a708a37
imusamasagar <usamaist2747@gmail.com>
2023-11-13 17:21:35 +0500

    Day 21

---
+++ Day 21 Practice Day.js
@@ -0,0 +1,10 @@
+// Day 21 Chapter 5 Practice Set
+
+// Question no 1
+// take input from user add it in array
+let arr1 = [12,54,36,87,5]
+console.log(arr1)
+let b = prompt("enter the no ")
+b= Number.parseInt(b)
+arr1.push(b)
+console.log(arr1)
