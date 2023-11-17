7d00593edf9b36c2de3845726d019b8301085e23
imusamasagar <usamaist2747@gmail.com>
2023-11-17 11:41:37 +0500

    Day 25

---
+++ Day 25 console methods.js/script.js
@@ -0,0 +1,6 @@
+// console se sare shoe ho jae ge jo jo items hote he iss k 
+console.log(console)
+// log bas msg ko print kar deta he cconsole me
+console.log("usama") 
+// info log jaise hi hota he bas separate folder me hota he
+console.info("usama here")
