6bc32d8057024e77e22adc26a29ddcb816334bd3
imusamasagar <usamaist2747@gmail.com>
2023-11-17 11:42:33 +0500

    Day 25

--- Day 25 console methods.js/script.js
+++ Day 25 console methods.js/script.js
@@ -9,3 +9,6 @@ console.assert(55<100)
 console.assert(55>100)
 // error red color se error show kar deta he simply bas
 console.error("dont touch")
+// warn warning show karta he 
+console.warn("i warn you")
+// time or timeend hum kisi bhi cheez ko kitna time lagta he wo bata deta he proccess likhn se pehle time laga lo or end par time end jaise k time or timend k arguments ka naam same hona chahiye 
