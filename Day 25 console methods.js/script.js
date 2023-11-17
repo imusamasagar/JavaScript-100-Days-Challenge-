947424cba8ed84baeed6a4ad9726561978444a0a
imusamasagar <usamaist2747@gmail.com>
2023-11-17 11:41:55 +0500

    Day 25

--- Day 25 console methods.js/script.js
+++ Day 25 console methods.js/script.js
@@ -4,3 +4,8 @@ console.log(console)
 console.log("usama") 
 // info log jaise hi hota he bas separate folder me hota he
 console.info("usama here")
+// assert means assertion ye condition check karta he agar true ho to kuch nhi ho warna red color se assertion failed ka msg a jata he
+console.assert(55<100)
+console.assert(55>100)
+// error red color se error show kar deta he simply bas
+console.error("dont touch")
