b2e7fbb68eb1492ee7cdbb4609d8b70db3e01e3f
imusamasagar <usamaist2747@gmail.com>
2023-11-18 21:04:23 +0500

    Day 26

--- Day 26 Guess Number Solution/script.js
+++ Day 26 Guess Number Solution/script.js
@@ -2,3 +2,10 @@ let a = Math.random() * 100;
 a = Number.parseInt(a)
 let inp;
 let score = 100;
+while (inp != a) {
+    score = score - 1;
+    inp = prompt("Enter the Number : ");
+    if (inp == a) {
+      console.log("Congradulation! You guess the correct number.")
+      console.log(`You guess the actual number in ${100 - score} chances.`)
+    }
