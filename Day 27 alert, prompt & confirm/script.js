772224269db3bfea607426933526ac585e495257
imusamasagar <usamaist2747@gmail.com>
2023-11-19 12:54:19 +0500

    Day 27

--- Day 27 alert, prompt & confirm/script.js
+++ Day 27 alert, prompt & confirm/script.js
@@ -8,3 +8,6 @@ let a = prompt("Enter a here", "578")
 // ye by default string hota he iss ko number me convert karne k liye parseInt ka function
 a = Number.parseInt(a)
 alert("You entered a of type " + (typeof a))
+// confirm use hota he jaise k agar kuch delete kar rahe ho tab confirmation lene k liye 
+let write = confirm("Do you want to write it to the page")
+// yaha if kaam kar raha he k agar confirm ko yes karo ge to if chale ga warna else
