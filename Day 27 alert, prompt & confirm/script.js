305e38dfeb486142e8d9a6a8d96dea9f4fbd6451
imusamasagar <usamaist2747@gmail.com>
2023-11-19 12:53:43 +0500

    Day 27

--- Day 27 alert, prompt & confirm/script.js
+++ Day 27 alert, prompt & confirm/script.js
@@ -4,3 +4,4 @@
 alert("Enter the value of a!")
 // prompt ye use hota he user se response lene k liye 
 // 578 yaha hum second default value de sakte he ye value by default likh kar ae gi iss ko change kar sakte he bby the way ap
+let a = prompt("Enter a here", "578")
