0b7b2b754ce7a7724b07258c47ea61151647754b
imusamasagar <usamaist2747@gmail.com>
2023-11-19 12:53:24 +0500

    Day 27

--- Day 27 alert, prompt & confirm/script.js
+++ Day 27 alert, prompt & confirm/script.js
@@ -2,3 +2,5 @@
 // mini window ko invoke karna 
 // its just a message for the user
 alert("Enter the value of a!")
+// prompt ye use hota he user se response lene k liye 
+// 578 yaha hum second default value de sakte he ye value by default likh kar ae gi iss ko change kar sakte he bby the way ap
