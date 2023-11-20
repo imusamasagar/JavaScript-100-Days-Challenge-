3d4f473ba6af7a956eaf59b3c34bfacde6109da3
imusamasagar <usamaist2747@gmail.com>
2023-11-20 11:39:59 +0500

    Day 28

---
+++ Day 28 window, BOM & DOM/script.js
@@ -0,0 +1,5 @@
+// window
+console.log(window)
+// aise bhi likh sakte he kisi bhi method ko window. kar k access kar sakte he 
+window.console.log(window)
+// ye global object hota he iss k andar 3 cheeze hoti he
