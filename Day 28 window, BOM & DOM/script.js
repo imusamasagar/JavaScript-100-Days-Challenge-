c10df8e52eead520d3dd07d4ced3484caaad7df2
imusamasagar <usamaist2747@gmail.com>
2023-11-20 11:40:20 +0500

    Day 28

--- Day 28 window, BOM & DOM/script.js
+++ Day 28 window, BOM & DOM/script.js
@@ -3,3 +3,9 @@ console.log(window)
 // aise bhi likh sakte he kisi bhi method ko window. kar k access kar sakte he 
 window.console.log(window)
 // ye global object hota he iss k andar 3 cheeze hoti he
+// 1 DOM
+// Document Object Model
+// html k content ko js k tor par dikkhati he 
+// ye css ko manipulate karne k liye bhi use hoti he lekijn tab jab ap chahte he k user k koi action se ap k page ki look me koi change a jae 
+// sara content as a html  
+console.log(document)
