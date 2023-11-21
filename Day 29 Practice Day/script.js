6752edef3455db7581269ba0835250dc643b9cf5
imusamasagar <usamaist2747@gmail.com>
2023-11-21 15:56:02 +0500

    Day 29

---
+++ Day 29 Practice Day/script.js
@@ -0,0 +1,13 @@
+// Question no 1
+// alert("Age required to tell you about driving so kindly cooperate")
+// let age = prompt("Enter your Age")
+// age = Number.parseInt(age)
+// if(age<18)
+// {
+//   alert("You can't drive")
+// }
+// else
+// {
+//   alert("You can drive")
+// }
+
