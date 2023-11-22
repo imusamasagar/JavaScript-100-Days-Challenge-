4de31e8df2a51be009d4677b1fcec0a48a3174c8
imusamasagar <usamaist2747@gmail.com>
2023-11-22 23:06:11 +0500

    Day 30

--- Day 30 Snake, Water & Gun game/script.js
+++ Day 30 Snake, Water & Gun game/script.js
@@ -6,3 +6,11 @@ do
   {
 let ui = prompt("press [S for snake] [W for water] [G for gun]");
 let ci = ch[Math.round(Math.random())];
+if(ui==ci)
+{
+  alert("Game Draw")
+}
+else if(ui=='S'&&ci=='W')
+{
+  alert("User Win")
+}
