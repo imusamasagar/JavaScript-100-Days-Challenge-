02401de78b00031521ad89decb9e75e9646132a1
imusamasagar <usamaist2747@gmail.com>
2023-11-23 05:30:48 +0500

    Day 31

---
+++ Day 31 Walking the DOM/script.js
@@ -0,0 +1,9 @@
+// DOM tree refers the html page where all nodes are objects
+// Three major types
+// 1- Text Nodes 
+// *** is the leaf of tree always
+// 2- Element Node
+// like the title tag head tag body tag are elements
+// 3- Comment Node
+// comments jo hote he html me <!--></--!>
+
