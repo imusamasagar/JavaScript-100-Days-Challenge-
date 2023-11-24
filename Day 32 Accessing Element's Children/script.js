d52b565ebfac160100f99c3093ed37b4f7bec8e1
imusamasagar <usamaist2747@gmail.com>
2023-11-24 22:10:11 +0500

    Day 32

---
+++ Day 32 Accessing Element's Children/script.js
@@ -0,0 +1,10 @@
+// html
+// html parent he 
+// head or body uss k child he 
+// head or body aik dusre k siblings he 
+
+// direct child 
+// child nodes wo hoti he jo direct link karti he parent root se
+// descendent nodes
+// nodes k nodes k nodes ye sab descendent nodes hote he 
+
