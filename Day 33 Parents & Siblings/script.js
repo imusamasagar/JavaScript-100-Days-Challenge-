ca1470442f8914d263e2f92630761965fe16ebbc
imusamasagar <usamaist2747@gmail.com>
2023-11-25 00:10:00 +0500

    Day 33

---
+++ Day 33 Parents & Siblings/script.js
@@ -0,0 +1,7 @@
+// DOM Manipulation 
+// ye read only hoti he aise nhi k ap inn k uper write kar de 
+// they are live k agar ap elem.chilNodes  variable k andar koi element add kar do $0 kar k to wo automatically update ho jae ga 
+// $0 iss ka mtlb he k agar ap kisi par click kar lo to uss k baad jab console par $0 likho to wo element ko refrence hota he or wo print ho jasta he issi tarha jis ko bhi select karo ge uss k baad console me uss ko $0 kar k access kar sakte he 
+// $1 bhi hota he wo previous selected element hota he
+// they are iterable using for --- of loop 
+
