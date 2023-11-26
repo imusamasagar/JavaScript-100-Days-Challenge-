455ca187a5b443aaaad8e8c846706d4df15db3aa
imusamasagar <usamaist2747@gmail.com>
2023-11-26 21:42:48 +0500

    Day 34

---
+++ Day 34 Elements only Navigation/script.js
@@ -0,0 +1,4 @@
+// hum ko pata he k jab ham firstChild likhe he ya sibling ko access karte he to aik masla atta he k wo text ya jo spacing hoti he uss ko child bata deta he 
+// agar hum ko dirsct element chahiye to kiya kare 
+// Element word use kare 
+
