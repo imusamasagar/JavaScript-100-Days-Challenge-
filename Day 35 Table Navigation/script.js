6459a1c0c87435d16ac8da27e05d72cf0045c6c8
imusamasagar <usamaist2747@gmail.com>
2023-11-27 14:09:55 +0500

    Day 35

---
+++ Day 35 Table Navigation/script.js
@@ -0,0 +1,4 @@
+// some dom elements may provide additional properties specific to their type for their connivance
+// lets see an example
+// table
+let table = document.body.firstElementChild.firstElementChild
