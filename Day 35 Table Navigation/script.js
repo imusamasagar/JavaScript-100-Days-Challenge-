1abbccfc35dfe48c6a47a576bd85256c17c4c6a2
imusamasagar <usamaist2747@gmail.com>
2023-11-27 14:10:13 +0500

    Day 35

--- Day 35 Table Navigation/script.js
+++ Day 35 Table Navigation/script.js
@@ -2,3 +2,8 @@
 // lets see an example
 // table
 let table = document.body.firstElementChild.firstElementChild
+console.log(table.rows) // collection of tr elements
+console.log(table.caption) // reference to <caption>
+console.log(table.thead) // referenceto <thead> 
+console.log(table.tfoot) // reference to <tfoot>
+console.log(table.tBodies) // collection of <tbody> elements
