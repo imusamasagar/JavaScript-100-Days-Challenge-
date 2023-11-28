693ce54026ad7d26be828f84bb962d058b6003f9
imusamasagar <usamaist2747@gmail.com>
2023-11-28 13:47:21 +0500

    Day 36

--- Day 36 DOM Searching/script.js
+++ Day 36 DOM Searching/script.js
@@ -11,3 +11,11 @@
 // id de de ge uss ko lekin aik baat yaad rakhe k aik id aik ko hi de sakte he 
 let t1 = document.getElementById("tt")
 t1.style.color = "red"
+// query selector all ye ap ko return kare ga all the elements inside the element matching the given CSS selectors
+// ab yaha . ae ga class k liye or # ae ga id k liye kiun k ye css selectors he 
+// ye sare wo elements de ga jin ki class given wali ho gi 
+let ct = document.querySelectorAll(".card-title")
+console.log(ct)
+ct[0].style.color= "cyan"
+ct[1].style.color = "blue"
+ct[2].style.color = "pink"
