fedaa87cfbaa26f86425cd1137b0fda7324d5fec
imusamasagar <usamaist2747@gmail.com>
2023-11-29 01:17:55 +0500

    Day 37

--- Day 37 Matches, Closet & Contains/script.js
+++ Day 37 Matches, Closet & Contains/script.js
@@ -2,3 +2,7 @@
 let id1 = document.getElementById("id1")
 let id2 = document.getElementById("id2")
 let sp2 = document.getElementById("sp2")
+// matches
+// elem.matches(css) to check if the element matches the given css selector
+// this will return true bcz id1 element matches the class c1
+console.log(id1.matches(".c1"))
