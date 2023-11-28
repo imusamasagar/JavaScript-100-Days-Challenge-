4a3aec3afa4daf1b4cc56e1fa72810f98f292fac
imusamasagar <usamaist2747@gmail.com>
2023-11-28 13:46:44 +0500

    Day 36

---
+++ Day 36 DOM Searching/script.js
@@ -0,0 +1,11 @@
+// DOM navigation properties are helpful when elements are close to each other to app log child parent sibling kar lete thy lekin agar ap ko pata nhi he element kaha he to phir masla tha to ye concept waha kaam atta he 
+
+// Searching the DOM
+// most used functions
+// accesed the lements by id calss names or css selectors ki madad se 
+// manipulation throw such access 
+
+// if someone asked you to change the color of first card title
+
+// throw id 
+// id de de ge uss ko lekin aik baat yaad rakhe k aik id aik ko hi de sakte he 
