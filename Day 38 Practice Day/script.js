3d17920d91c8e2952092532af59040338bed5eb2
imusamasagar <usamaist2747@gmail.com>
2023-12-06 22:01:15 +0500

    Day 38

--- Day 38 Practice Day/script.js
+++ Day 38 Practice Day/script.js
@@ -3,3 +3,8 @@
 let e1 = document.body.firstElementChild.firstElementChild.firstElementChild
 e1.style.color = "red" 
 
+// Question 2
+// create table without tbody check if the view sorce page show tbody or not
+// answer no he bcz view source page hum ko raw code dikhata he wo correction nhi karta code ki 
+// lekin agar ap inspect par jae ge dekhe ge to browser ne tbody daal di ho gi bcz as we study eralier browser have a capability to do auto correction
+
