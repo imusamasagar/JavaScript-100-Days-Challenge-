3adae92bfeeaea1ca8d20e478e75f5617fd07b02
imusamasagar <usamaist2747@gmail.com>
2023-12-06 22:01:36 +0500

    Day 38

--- Day 38 Practice Day/script.js
+++ Day 38 Practice Day/script.js
@@ -8,3 +8,16 @@ e1.style.color = "red"
 // answer no he bcz view source page hum ko raw code dikhata he wo correction nhi karta code ki 
 // lekin agar ap inspect par jae ge dekhe ge to browser ne tbody daal di ho gi bcz as we study eralier browser have a capability to do auto correction
 
+// Question no 3
+// change the color of first and last child to green
+let c1 = document.body.firstElementChild.firstElementChild.firstElementChild
+let c3 = document.body.firstElementChild.firstElementChild.lastElementChild
+c1.style.color = "green"
+c3.style.color = "green"
+
+// Question no 4
+// change background of all li
+Array.from(document.getElementsByTagName("li")).forEach((element) =>{
+  element.style.background = "cyan"
+})
+
